d6b7e3f32edd20f44ee0caa05ca8776aeeca144a
Renato <xrenatomno@gmail.com>
2022-03-29 13:36:27 -0300

    ADD GlobalStyle component

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import {GlobalStyle} from './styles/global';
 
-function App() {
+
+const Title = styled.h1`
+  color: red;
+  font-size: 64px;
+`
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title>coe galera</Title>
+      <GlobalStyle />
     </div>
   );
 }
-
-export default App;
